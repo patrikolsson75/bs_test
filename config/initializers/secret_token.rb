64cb6504ed84f7b3116dc272cf5aad7608db2955
Patrik Olsson <patrik_olsson@mac.com>
2012-02-18 12:13:33 +0100

    Added Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BsTest::Application.config.secret_token = 'f2a51a1daf0592319c40e332f0672dc911ac2fd85466ecd0ef2fcb603061e785592b031f5256b930bc256df382734ab51bbd31c0c7c1ab7b3cf6867cdee26b38'
